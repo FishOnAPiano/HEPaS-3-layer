60b72469c58ca1f12be7dfe96914c930e89f3411
FishOnAPiano <104439815+FishOnAPiano@users.noreply.github.com>
2022-05-23 20:41:31 +0800

    Corrected error in "other student" data entry

--- client.py
+++ client.py
@@ -28,43 +28,42 @@ if EOUStudent == "y":
 else:
     #Enter new data
     personID = input("Please enter your Person ID: ")
+    print("Please enter 12-30 unit codes and marks, and enter 'done' when complete")
+    unitMarkList = []
     while True:
-        print("Please enter 12-30 unit codes and marks, and enter 'done' when complete")
-        unitMarkList = []
-        while True:
-            unitCode = input("Unit code: ")
-            if unitCode == "done":
-                break
-            markString = input("Mark: ")
-            mark = -1
-            try:
-                mark = int(markString)
-            except ValueError:
-                pass
-            
-            numPasses = 0
-            numFails = 0
-            for unitMark in unitMarkList:
-                if unitMark[0] == unitCode:
-                    if unitMark[1] >= 50:
-                        numPasses += 1
-                    else:
-                        numFails +=1
-            if mark < 0 or mark > 100:
-                #Catches non-number input as well, as if cannot be converted to number mark will still be -1
-                print("Invalid mark, please enter a number between 0 and 100")
-            elif not unitCode:
-                print("Please enter a valid unit code")
-            elif numPasses == 1 and mark >= 50:
-                print(unitCode + " has already had a passing mark recorded, only one passing mark is allowed")
-            elif numFails == 2 and mark < 50:
-                print(unitCode + " already has two failing marks, only two failing marks are allowed")
-            else:
-                unitMarkList.append((unitCode, mark))
-                if len(unitMarkList) == 30:
-                    print("Maximum of 30 marks reached, evaluating using provided marks")
-                    break                    
-    if len(unitMarkList >= 12):
+        unitCode = input("Unit code: ")
+        if unitCode == "done":
+            break
+        markString = input("Mark: ")
+        mark = -1
+        try:
+            mark = int(markString)
+        except ValueError:
+            pass
+        
+        numPasses = 0
+        numFails = 0
+        for unitMark in unitMarkList:
+            if unitMark[0] == unitCode:
+                if unitMark[1] >= 50:
+                    numPasses += 1
+                else:
+                    numFails +=1
+        if mark < 0 or mark > 100:
+            #Catches non-number input as well, as if cannot be converted to number mark will still be -1
+            print("Invalid mark, please enter a number between 0 and 100")
+        elif not unitCode:
+            print("Please enter a valid unit code")
+        elif numPasses == 1 and mark >= 50:
+            print(unitCode + " has already had a passing mark recorded, only one passing mark is allowed")
+        elif numFails == 2 and mark < 50:
+            print(unitCode + " already has two failing marks, only two failing marks are allowed")
+        else:
+            unitMarkList.append((unitCode, mark))
+            if len(unitMarkList) == 30:
+                print("Maximum of 30 marks reached, evaluating using provided marks")
+                break                    
+    if len(unitMarkList) >= 12:
         #Server call
         print(proxy.evaluateQualification(personID, unitMarkList))
     else:
